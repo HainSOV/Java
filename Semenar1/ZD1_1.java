dfd4560cec27616a64d77cb96034ce080c93e308
Mikail Sobolev <ya@hsov.ru>
2024-03-27 20:17:29 +0600

    work_at_home_270324

--- Semenar1/ZD1_1.java
+++ Semenar1/ZD1_1.java
@@ -1,20 +1,27 @@
 package Semenar1;
-
-import java.time.LocalTime;
-import java.util.Date;
 import java.util.Scanner;
+import java.time.LocalTime;
 
-public class ZD1_1{
+public class ZD1_1 {
     public static void main(String[] args) {
-        LocalTime nowTime = LocalTime.now();
-        Scanner namScanner = new Scanner(System.in);
-        System.out.printf("Ведите имя: ", "\n");
-        String name = namScanner.nextLine();
-        namScanner.close();
-        Date date = new Date();
-        System.out.println(date.toString());
-        }
-        
-       
-}
+        Scanner scanner = new Scanner(System.in);
 
+        // Запрос имени пользователя
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
+
+        // Получение текущего времени
+        LocalTime currentTime = LocalTime.now();
+
+        // Вывод приветствия в зависимости от текущего времени
+        if (currentTime.isAfter(LocalTime.of(5, 0)) && currentTime.isBefore(LocalTime.of(11, 59))) {
+            System.out.println("Доброе утро, " + name + "!");
+        } else if (currentTime.isAfter(LocalTime.of(12, 0)) && currentTime.isBefore(LocalTime.of(17, 59))) {
+            System.out.println("Добрый день, " + name + "!");
+        } else if (currentTime.isAfter(LocalTime.of(18, 0)) && currentTime.isBefore(LocalTime.of(22, 59))) {
+            System.out.println("Добрый вечер, " + name + "!");
+        } else {
+            System.out.println("Доброй ночи, " + name + "!");
+        }
+    }
+}
